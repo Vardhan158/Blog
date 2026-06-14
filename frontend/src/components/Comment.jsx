5aa01118b281ebab8077dc1a10703fd7bbb7b50e
Vardhan158 <harshavardhandevang@gmail.com>
2026-06-14 18:27:01 +0530

    Update API_URL to production backend

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -3,7 +3,7 @@ import { FaHeart } from "react-icons/fa";
 import axios from "axios";
 import { getToken } from "../utils/authStorage";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "https://blog-backend-omyx.onrender.com";
 const DEFAULT_AVATAR = "https://cdn-icons-png.flaticon.com/512/847/847969.png";
 
 const getProfilePic = (user) => {
